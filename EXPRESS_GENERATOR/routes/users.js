c9d1b7055f0dcc8561a88c68010c2ce5be19acdb
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2019-12-02 09:17:54 +0000

    esercizio 03.1 completo

--- EXPRESS_GENERATOR/routes/users.js
+++ EXPRESS_GENERATOR/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/ciaone', function(req, res, next) {
+  res.send('PRONTONE!!!!!!');
+});
+
+
 module.exports = router;
